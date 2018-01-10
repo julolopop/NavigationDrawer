7396405dcfb42693fa241f98ac7f0e4900ce8c0b
alfchaval <alfchaval@hotmail.com>
2018-01-10 14:44:44 +0100

    2

--- app/src/main/java/com/example/usuario/navigatordrawer/MainActivity.java
+++ app/src/main/java/com/example/usuario/navigatordrawer/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.usuario.navigatordrawer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,23 +13,69 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
         toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation_view);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupNavigationView();
+    }
+
+    /**
+     * Método que inicializa el Listener NavigationItemSelected, y en base a la opción seleccionada se realiza una acción
+     */
+    private void setupNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Log.d("navigatioDrawer", "Se ha pulsado la opción home");
+                        break;
+                    case R.id.action_dependency:
+                        Log.d("navigatioDrawer", "Se ha pulsado la opción dependency");
+                        break;
+                    case R.id.action_sector:
+                        Log.d("navigatioDrawer", "Se ha pulsado la opción sector");
+                        break;
+                    case R.id.action_settings:
+                        Log.d("navigatioDrawer", "Se ha pulsado la opción settings");
+                        break;
+                    case R.id.action_help:
+                        Log.d("navigatioDrawer", "Se ha pulsado la opción help");
+                        break;
+                }
+                item.setChecked(true);
+                getSupportActionBar().setTitle(item.getTitle());
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Log.d("NavigatorDrawer", "Se ha pulsado el icono home");
+                //Log.d("NavigatorDrawer", "Se ha pulsado el icono home");
+                drawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }

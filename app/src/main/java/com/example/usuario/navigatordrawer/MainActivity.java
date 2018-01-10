7236bf144d189167c4d1a3037dd81858c611248c
alfchaval <alfchaval@hotmail.com>
2018-01-10 13:45:22 +0100

    first commit

---
+++ app/src/main/java/com/example/usuario/navigatordrawer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.usuario.navigatordrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.d("NavigatorDrawer", "Se ha pulsado el icono home");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
